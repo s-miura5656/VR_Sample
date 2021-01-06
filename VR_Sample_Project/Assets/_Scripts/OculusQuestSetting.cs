0c8d1ab4a7fcc2c7bd4f0fd5b33a6930fdab1028
souta_miura <miura042910@gmail.com>
2021-01-06 22:48:51 +0900

    feat:FPSを上げるために画面解像度を調整する機能を追加
    perf:ユニティちゃんのMaterialの設定を変更

---
+++ VR_Sample_Project/Assets/_Scripts/OculusQuestSetting.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class OculusQuestSetting : MonoBehaviour
+{
+    [Header("画面の解像度")]
+    [SerializeField] private float textureResolution = 1.0f;
+
+    private void Awake()
+    {
+        XRSettings.eyeTextureResolutionScale = textureResolution;
+    }
+}
